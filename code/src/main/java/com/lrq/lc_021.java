a7e2de23d8623687a823a11cc07b7f7fb66ec519
lllrrq <89633145+lllrrq@users.noreply.github.com>
2021-10-09 10:05:17 +0800

    notes

--- code/src/main/java/com/lrq/lc_021.java
+++ code/src/main/java/com/lrq/lc_021.java
@@ -1,16 +1,17 @@
 package com.lrq;
 
 /*
-    深入理解归并排序
+    题目描述：将两个有序的链表合并为一个有序的链表
+    思路：定义一个ListNode类，实现归并排序中最后一步两两合并过程
  */
 public class lc_021 {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         // 类似归并排序中的合并过程
-        ListNode dummyHead = new ListNode(0);
+        ListNode dummyHead = new ListNode(0); //生成的新的链表
         ListNode cur = dummyHead;
         while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
+            if (l1.val < l2.val) {   //从比较两个链表中第一个节点的大小开始
                 cur.next = l1;
                 cur = cur.next;
                 l1 = l1.next;
@@ -20,7 +21,7 @@ public class lc_021 {
                 l2 = l2.next;
             }
         }
-        // 任一为空，直接连接另一条链表
+        // 任一为空(本来就是空链表或者原本的节点全部移到新链表中)，直接连接另一条链表
         if (l1 == null) {
             cur.next = l2;
         } else {
